e46e8d58b4a17596936b7bb242c55f7453eb425b
Gregorii work <tijester@ukr.net>
2020-06-02 17:29:08 +0300

    Video View

--- app/src/main/java/com/udemy/usingvideo/MainActivity.java
+++ app/src/main/java/com/udemy/usingvideo/MainActivity.java
@@ -2,7 +2,12 @@ package com.udemy.usingvideo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String videoSours = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4?";
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demo);
+        //videoView.setVideoURI(Uri.parse(videoSours));
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
     }
 }
